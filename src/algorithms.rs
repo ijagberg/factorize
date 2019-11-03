b0ff961a26be92b9c0bb019272d6af461b72228a
Isak Jägberg <ijagberg@gmail.com>
2019-11-03 15:46:08 +0100

    Add a test, implement structopt for arg parsing

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -4,9 +4,9 @@ pub fn trial_division(mut n: u128) -> Vec<u128> {
     if n == 0 {
         return factors;
     }
-    
+
     let candidates = iter::once(2).chain((3..).step_by(2));
-    
+
     for candidate in candidates {
         if n <= 1 {
             break;
@@ -20,3 +20,21 @@ pub fn trial_division(mut n: u128) -> Vec<u128> {
     factors
 }
 
+#[test]
+fn trial_div_should_factorize_correctly() {
+    let numbers: Vec<u128> = vec![
+        1,
+        2,
+        3,
+        10,
+        100,
+        150,
+        50000,
+        1_234_567_890,
+        948_347_928_371_874,
+    ];
+    for number in numbers {
+        let factors = trial_division(number);
+        assert_eq!(number, factors.iter().product());
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
-use std::env;
 use std::time;
+use structopt::StructOpt;
 pub mod algorithms;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut numbers: Vec<u128> = Vec::new();
-    for arg in &args[1..] {
-        match arg.parse::<u128>() {
-            Ok(n) => numbers.push(n),
-            Err(e) => {
-                eprintln!("An error occurred while parsing: {:?}", e);
-                return;
-            }
-        };
-    }
+#[derive(StructOpt, Debug)]
+#[structopt(name = "basic")]
+struct Options {
+    numbers: Vec<u128>,
+}
 
-    for number in numbers {
+fn main() {
+    let opts = Options::from_args();
+
+    for number in opts.numbers {
         let timer = time::Instant::now();
         let factors = algorithms::trial_division(number);
-        let mut product: u128 = 1;
-        for factor in &factors {
-            product *= factor;
-        }
         println!("{} => {:?}, took {:?}", number, factors, timer.elapsed());
-        assert_eq!(number, product);
     }
 }
