4ff22490ecd0cfd79dbce1a56dc67cf61d3c97f7
Isak Jägberg <ijagberg@gmail.com>
2019-11-14 20:52:17 +0100

    Add tests for high primes/composites

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -67,6 +67,18 @@ mod tests {
 
     const LOW_PRIMES: [u128; 10] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
     const LOW_COMPOSITES: [u128; 10] = [1, 4, 6, 8, 9, 10, 12, 14, 15, 16];
+    const HIGH_PRIMES: [u128; 4] = [
+        613_304_922_953,
+        344_501_616_449,
+        342_907_683_289,
+        885_027_563_087,
+    ];
+    const HIGH_COMPOSITES: [u128; 4] = [
+        885_027_563_083,
+        1_440_456_103_525,
+        1_439_896_263_162,
+        2_287_220_812_551,
+    ];
 
     #[test]
     fn miller_rabin_accurate_for_low_primes() {
@@ -81,6 +93,26 @@ mod tests {
     #[test]
     fn miller_rabin_accurate_for_low_composites() {
         for composite in LOW_COMPOSITES.iter().map(|&n| Integer::from(n)) {
+            assert_eq!(
+                (&composite, miller_rabin(&composite.clone(), 40)),
+                (&composite, MillerRabinResult::Composite),
+            );
+        }
+    }
+
+    #[test]
+    fn miller_rabin_accurate_for_high_primes() {
+        for prime in HIGH_PRIMES.iter().map(|&n| Integer::from(n)) {
+            assert_eq!(
+                (&prime, miller_rabin(&prime.clone(), 40)),
+                (&prime, MillerRabinResult::ProbablyPrime)
+            );
+        }
+    }
+
+    #[test]
+    fn miller_rabin_accurate_for_high_composites() {
+        for composite in HIGH_COMPOSITES.iter().map(|&n| Integer::from(n)) {
             assert_eq!(
                 (&composite, miller_rabin(&composite.clone(), 40)),
                 (&composite, MillerRabinResult::Composite)
